859d6e1541038ed2390ef50e6ef16263c6d13b2b
Denny <ptreasuresoftsolution@gmail.com>
2022-04-22 11:08:18 +0530

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivitySideMenuAd.java
@@ -0,0 +1,14 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivitySideMenuAd extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_side_menu_ad);
+    }
+}
